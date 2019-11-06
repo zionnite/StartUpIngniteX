67df375a674f4ff6273ee040be203c38940cafe8
zionnite <nosakharee.16@gmail.com>
2019-11-06 17:08:18 +0100

    Just an update on type of User Category

---
+++ app/src/main/java/xyz/esuku/startupingnitex/Controller/Settings/About.java
@@ -0,0 +1,14 @@
+package xyz.esuku.startupingnitex.Controller.Settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import xyz.esuku.startupingnitex.R;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
